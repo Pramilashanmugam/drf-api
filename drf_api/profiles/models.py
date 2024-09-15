9036d74d809235a0d311016514b90a9b7bf0b5c1
Pramila Shanmugam <pramila.sl@gmail.com>
2024-09-15 20:18:24 +0000

    cloudinary connected to app

--- drf_api/profiles/models.py
+++ drf_api/profiles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 class Profile(models.Model):
@@ -10,7 +11,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../earth-3537401_640_cbeub3'
+        upload_to='images/', default='../earth-3537401_640_cbeub3.jpg'
     )
 
     class Meta:
